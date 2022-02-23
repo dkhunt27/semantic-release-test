ad82ac8381300c9118e3dee3af1f07d606cabb5e
Dan Hunt <dkhunt27@gmail.com>
2022-02-23 11:46:32 -0500

    fix: wip

---
+++ sr.sh
@@ -0,0 +1,11 @@
+echo "nextRelease type ${nextRelease.type}"
+echo "nextRelease version ${nextRelease.version}"
+echo "nextRelease gitHead ${nextRelease.gitHead}"
+echo "nextRelease gitTag ${nextRelease.gitTag}"
+echo "nextRelease notes ${nextRelease.notes}"
+
+echo "lastRelease version ${lastRelease.version}"
+echo "lastRelease gitHead ${lastRelease.gitHead}"
+echo "lastRelease gitTag ${lastRelease.gitTag}"
+
+echo "commits length ${commits.length}"
